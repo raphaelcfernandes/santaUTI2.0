4d3ba45254b23250b7f8cf1c4b3eff43dac5a54a
Raphael Cardoso Fernandes <raphael.cardoso.f@gmail.com>
2017-12-03 18:17:16 -0200

    Apenas bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import {Subscription} from 'rxjs/Subscription';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit, OnDestroy {
